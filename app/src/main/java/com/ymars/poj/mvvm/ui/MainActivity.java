b4842011e645bed94057141708de304f96d34e01
Ymars1990 <651717448@qq.com>
2020-06-20 09:36:33 +0800

    Main框架界面

--- app/src/main/java/com/ymars/poj/mvvm/ui/MainActivity.java
+++ app/src/main/java/com/ymars/poj/mvvm/ui/MainActivity.java
@@ -2,10 +2,7 @@ package com.ymars.poj.mvvm.ui;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
